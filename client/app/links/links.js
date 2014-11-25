7d3fb82852a4c19b3d1911402ca7ea5b22d3e014
brianjscoles <brianscoles@gmail.com>
2014-11-24 17:23:42 -0800

    Pass all tests, disable Karma animations, enable routing.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,23 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
-  // Your code here
+.controller('LinksController', function ($scope, $http, Links) {
+  $scope.data = {};
+  $scope.getLinks = function(){
+    alert('this is getLinks');
+    //get links from mongodb
+    //callback will set data array equal to links from db
+    $http.get('/api/links')
+      .then(function(response){
+        console.log(response.data);
+        $scope.data.links = response.data;
+      })
+      .catch(function(error){
+        throw error;
+      });
+
+  };
+  $scope.getLinks();
+  // SHOULD HAVE: .data, .getLinks()
+  // contains some logic
+  //
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,20 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $location, $http, Links) {
+  $scope.link = {};
+  $scope.addLink = function(){
+    console.log('this is addlinks in shortencontroller');
+    //add links to somewhere
+    $http.post('/api/links',$scope.link)
+      .then(function(response){
+        console.log('RESPONSE FROM POST! ' + response.status);
+      })
+      .catch(function(error){
+        throw error;
+      });
+
+  };
+
+
   // Your code here
 });
